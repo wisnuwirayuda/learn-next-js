58aa9a914984b26890d1df9dfac67388a216ee3e
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-23 23:08:06 +0700

    feat: outputting dynamic values

--- Bagian 2 - React Refresher/react-crash-course/src/components/Post.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/Post.jsx
@@ -1,6 +1,10 @@
+const names = ['Wisnu', 'Trenggono'];
+
 function Post() {
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
     return <div>
-        <p>Wisnu</p>
+        <p>{chosenName}</p>
         <p>React.js is awesome!</p>
     </div>
 }
