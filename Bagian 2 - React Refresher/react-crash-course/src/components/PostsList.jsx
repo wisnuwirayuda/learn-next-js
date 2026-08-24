7a3f21754f73dc3503f32d3d4c26ba48f06018d5
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-24 23:44:30 +0700

    feat: preparing the app for state management

--- Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Wisnu" body="React.js is awesome!" />
-            <Post author="Trenggono" body="Check out the full course!" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Wisnu" body="React.js is awesome!" />
+                <Post author="Trenggono" body="Check out the full course!" />
+            </ul>
+        </>
     )
 }
 
