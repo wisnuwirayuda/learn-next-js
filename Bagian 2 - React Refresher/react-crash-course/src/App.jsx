75ba5e035367aef2e8b0b45b94b2f0efd0558b9e
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-24 21:41:37 +0700

    feat: reusing components

--- Bagian 2 - React Refresher/react-crash-course/src/App.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/App.jsx
@@ -1,7 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return <main>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </main>;
 }
 
 export default App;
