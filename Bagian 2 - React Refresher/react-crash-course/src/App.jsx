240f273e5cda0ae63d0856703a2d8144534a7a98
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-24 22:19:37 +0700

    feat: passing data to components with props

--- Bagian 2 - React Refresher/react-crash-course/src/App.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/App.jsx
@@ -1,12 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <main>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-  </main>;
+  return (
+    <main>
+      <Post author="Wisnu" body="React.js is awesome!" />
+      <Post author="Trenggono" body="Check out the full course!" />
+    </main>
+  );
 }
 
 export default App;
--- Bagian 2 - React Refresher/react-crash-course/src/components/Post.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/Post.jsx
@@ -1,12 +1,10 @@
-const names = ['Wisnu', 'Trenggono'];
-
-function Post() {
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
-    return <div>
-        <p>{chosenName}</p>
-        <p>React.js is awesome!</p>
-    </div>
+function Post(props) {
+    return (
+        <div>
+            <p>{props.author}</p>
+            <p>{props.body}</p>
+        </div>
+    )
 }
 
 export default Post;
