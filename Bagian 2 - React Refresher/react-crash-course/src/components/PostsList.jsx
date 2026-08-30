0c630b81790a81bd374676d72db166f4345cff48
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-30 11:11:43 +0700

    feat: adding a shared header & more state management

--- Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -4,14 +4,9 @@ import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
-function PostsList() {
-    const [modalIsVisible, setModalIsVisible] = useState(true);
+function PostsList({ isPosting, onStopPosting}) {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function hideModalHandler() {
-        setModalIsVisible(false);
-    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -36,8 +31,8 @@ function PostsList() {
 
     return (
         <>
-            {modalIsVisible && (
-                <Modal onClose={hideModalHandler}>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
                     <NewPost 
                         onBodyChange={bodyChangeHandler} 
                         onAuthorChange={authorChangeHandler} 
