75aefd2acbd31fdab15181183c101e511355ec0c
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-30 10:42:28 +0700

    feat: state & conditional content

--- Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -16,14 +21,29 @@ function PostsList() {
         setEnteredAuthor(event.target.value);
     }
 
+    // let modalContent;
+
+    // if (modalIsVisible) {
+    //     modalContent = (
+    //         <Modal onClose={hideModalHandler}>
+    //             <NewPost 
+    //                 onBodyChange={bodyChangeHandler} 
+    //                 onAuthorChange={authorChangeHandler} 
+    //             />
+    //         </Modal>
+    //     );
+    // }
+
     return (
         <>
-            <Modal>
-                <NewPost 
-                    onBodyChange={bodyChangeHandler} 
-                    onAuthorChange={authorChangeHandler} 
-                />
-            </Modal>
+            {modalIsVisible && (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost 
+                        onBodyChange={bodyChangeHandler} 
+                        onAuthorChange={authorChangeHandler} 
+                    />
+                </Modal>
+            )}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Trenggono" body="Check out the full course!" />
