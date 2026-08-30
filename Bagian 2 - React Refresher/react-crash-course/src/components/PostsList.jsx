ef5d88897399e4dddabb3793045225894bb8eee7
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-30 19:35:12 +0700

    feat: handling form submissions

--- Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -1,47 +1,17 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting}) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
-    // let modalContent;
-
-    // if (modalIsVisible) {
-    //     modalContent = (
-    //         <Modal onClose={hideModalHandler}>
-    //             <NewPost 
-    //                 onBodyChange={bodyChangeHandler} 
-    //                 onAuthorChange={authorChangeHandler} 
-    //             />
-    //         </Modal>
-    //     );
-    // }
-
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
-                    <NewPost 
-                        onBodyChange={bodyChangeHandler} 
-                        onAuthorChange={authorChangeHandler} 
-                        onCancel={onStopPosting}
-                    />
+                    <NewPost onCancel={onStopPosting} />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Trenggono" body="Check out the full course!" />
             </ul>
         </>
