990e29a859073ad217c78f383f3413f80a042cc0
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-31 21:25:04 +0700

    feat: updating state based on previous state

--- Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -2,13 +2,20 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
+import { useState } from "react";
 
-function PostsList({ isPosting, onStopPosting}) {
+function PostsList({ isPosting, onStopPosting }) {
+    const [posts, setPosts] = useState([]);
+
+    function addPostHandler(postData) {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
+
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
-                    <NewPost onCancel={onStopPosting} />
+                    <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
                 </Modal>
             )}
             <ul className={classes.posts}>
