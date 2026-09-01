8e92d33c16ff371d1fb5b122a95fb9bd7c0b974f
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-02 01:17:35 +0700

    feat: handle loading state

--- Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import { useState, useEffect } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts');
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
 
         fetchPosts();
@@ -35,15 +38,22 @@ function PostsList({ isPosting, onStopPosting }) {
                     <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
                 </Modal>
             )}
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>
             )}
-            {posts.length === 0 && (
+            {!isFetching && posts.length === 0 && (
                 <div style={{textAlign: 'center', color: 'white'}}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
+                </div>
+            )}
+            {isFetching && (
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <p>
+                        Loading posts...
+                    </p>
                 </div>
             )}
         </>
