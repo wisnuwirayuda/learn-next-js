7b4d0f9715049dee8999fbbf341af4aafca24d5b
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-02 01:30:42 +0700

    feat: adding routes

--- Bagian 2 - React Refresher/react-crash-course/src/main.jsx
+++ Bagian 2 - React Refresher/react-crash-course/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import NewPost from './components/NewPost'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/create-post',
+    element: <NewPost />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
