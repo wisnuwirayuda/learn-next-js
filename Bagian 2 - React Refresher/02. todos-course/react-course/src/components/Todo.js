a4158521be3cf09237e2f25bc64d93a360fc0356
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-09 20:23:27 +0700

    feat: working with event props

--- Bagian 2 - React Refresher/02. todos-course/react-course/src/components/Todo.js
+++ Bagian 2 - React Refresher/02. todos-course/react-course/src/components/Todo.js
@@ -8,6 +8,10 @@ function Todo(props) {
     function deleteHandler() {
         setModalIsOpen(true);
     }
+
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
     
     return (
         <div className="card">
@@ -15,8 +19,8 @@ function Todo(props) {
             <div className="actions">
                 <button className="btn" onClick={deleteHandler}>Delete</button>
             </div>
-            {modalIsOpen && <Modal />}
-            {modalIsOpen && <Backdrop />}
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+            {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
         </div>
     )
 }
