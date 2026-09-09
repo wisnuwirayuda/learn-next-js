f1c9b3554f740f54f62bcc7257ec9e9fd69fe5d7
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-09 14:05:07 +0700

    feat: introducing state

--- Bagian 2 - React Refresher/02. todos-course/react-course/src/components/Todo.js
+++ Bagian 2 - React Refresher/02. todos-course/react-course/src/components/Todo.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     function deleteHandler() {
-        console.log(props.text);
+        setModalIsOpen(true);
     }
     
     return (
@@ -9,6 +15,8 @@ function Todo(props) {
             <div className="actions">
                 <button className="btn" onClick={deleteHandler}>Delete</button>
             </div>
+            {modalIsOpen && <Modal />}
+            {modalIsOpen && <Backdrop />}
         </div>
     )
 }
