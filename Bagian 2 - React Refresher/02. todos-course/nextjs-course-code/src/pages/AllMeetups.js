bc59a94221fc54deebeb47b71804603efdc4d6ed
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-09 23:10:48 +0700

    feat: adding even more components

--- Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/AllMeetups.js
+++ Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/AllMeetups.js
@@ -1,9 +1,11 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
   {
     id: 'm1',
     title: 'This is a first meetup',
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHYYYOiIQRsOFj7BExp3QJYcEm35UeaTneEmqJiokFFlkIQm8nwaFRSWo&s=10',
     address: 'Meetupstreet 5, 12345 Meetup City',
     description:
       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
@@ -12,7 +14,7 @@ const DUMMY_DATA = [
     id: 'm2',
     title: 'This is a second meetup',
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHYYYOiIQRsOFj7BExp3QJYcEm35UeaTneEmqJiokFFlkIQm8nwaFRSWo&s=10',
     address: 'Meetupstreet 5, 12345 Meetup City',
     description:
       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
@@ -23,15 +25,7 @@ function AllMeetupsPage() {
     return (
         <section>
             <h1>All Meetups</h1>
-            <ul>
-                {DUMMY_DATA.map((meetup) => {
-                    return (
-                        <li key={meetup.id}>
-                            {meetup.title}
-                        </li>
-                    )
-                })}
-            </ul>
+            <MeetupList meetups={DUMMY_DATA} />
         </section>
     )
 }
