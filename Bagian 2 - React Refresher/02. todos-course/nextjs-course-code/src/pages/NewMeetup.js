b36fe858df8fd3ebbf9b1049e7b4d198d89b9ac9
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-11 23:07:37 +0700

    feat: navigating programmatically

--- Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/NewMeetup.js
+++ Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useHistory();
+
     function addMeetupHandler(meetupData) {
         fetch(
             'https://react-getting-started-b2ada-default-rtdb.firebaseio.com/meetups.json',
@@ -11,7 +14,9 @@ function NewMeetupPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() => {
+            history.replace('/');
+        });
     }
 
     return (
