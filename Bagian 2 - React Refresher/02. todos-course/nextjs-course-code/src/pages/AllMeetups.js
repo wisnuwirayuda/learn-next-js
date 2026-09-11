8f2ebc68889bff15d704ee0fe70e6abba6c5a17e
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-11 23:44:05 +0700

    feat: getting started with fetching data

--- Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/AllMeetups.js
+++ Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/AllMeetups.js
@@ -1,33 +1,33 @@
+import { useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHYYYOiIQRsOFj7BExp3QJYcEm35UeaTneEmqJiokFFlkIQm8nwaFRSWo&s=10',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHYYYOiIQRsOFj7BExp3QJYcEm35UeaTneEmqJiokFFlkIQm8nwaFRSWo&s=10',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
-
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  fetch(
+    'https://react-getting-started-b2ada-default-rtdb.firebaseio.com/meetups.json'
+  ).then(response => {
+    return response.json();
+  }).then(data => {
+    setIsLoading(false);
+    setLoadedMeetups(data);
+  });
+
+  if (isLoading) {
     return (
-        <section>
-            <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
-        </section>
+      <section>
+        <p>Loading...</p>
+      </section>
     )
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  )
 }
 
 export default AllMeetupsPage;
