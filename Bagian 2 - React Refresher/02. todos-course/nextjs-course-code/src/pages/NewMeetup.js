989de0111a3efef29ebcd0f698951be06ca55c46
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-11 23:00:40 +0700

    feat: sending a POST http request

--- Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/NewMeetup.js
+++ Bagian 2 - React Refresher/02. todos-course/nextjs-course-code/src/pages/NewMeetup.js
@@ -2,7 +2,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
     function addMeetupHandler(meetupData) {
-        
+        fetch(
+            'https://react-getting-started-b2ada-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
     }
 
     return (
