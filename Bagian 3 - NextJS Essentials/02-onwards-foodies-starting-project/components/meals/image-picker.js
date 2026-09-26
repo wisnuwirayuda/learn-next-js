1a70a7b73ec50de2a2be9567f52649efb482c1dd
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-26 20:40:37 +0700

    feat: adding an image preview to the picker

--- Bagian 3 - NextJS Essentials/02-onwards-foodies-starting-project/components/meals/image-picker.js
+++ Bagian 3 - NextJS Essentials/02-onwards-foodies-starting-project/components/meals/image-picker.js
@@ -1,19 +1,45 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
     const imageInputRef = useRef();
 
     function handlePickClick() {
         imageInputRef.current.click();
     }
 
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = (url) => {
+            setPickedImage(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    }
+
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && (
+                        <Image 
+                            src={pickedImage} 
+                            alt="The image selected by the user." 
+                            fill 
+                        />
+                    )}
+                </div>
                 <input 
                     className={classes.input}
                     type="file" 
@@ -21,6 +47,7 @@ export default function ImagePicker({ label, name }) {
                     accept="image/png, image/jpeg" 
                     name={name}
                     ref={imageInputRef}
+                    onChange={handleImageChange}
                 />
                 <button className={classes.button} type="button" onClick={handlePickClick}>
                     Pick an Image
